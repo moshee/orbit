984d28cf3fbb3994a87ea4ce9994184254a9f545
teh moshee <marshall.meng@gmail.com>
2011-07-24 22:50:54 -0700

    still getting the hang of git, lol

--- bot.rb
+++ bot.rb
@@ -1,28 +1,24 @@
 # encoding: UTF-8
 
-%w{json net/http socket hpricot cgi}.each {|i| require i}
+%w{json net/http socket hpricot cgi htmlentities}.each { |r| require r }
 
 $prefixes = ["]", "."]
-$api = {
-	:imgur	=>	{
-		:key	=>	"86479b06ff689612409190829dd46576",
-		:url	=>	"http://api.imgur.com/2/upload.json"
-	}
+$api_keys = {
+	:imgur		=>	"",
+	:dictionary	=>	""
 }
-
 $byte_killer = Iconv.new('UTF-8//IGNORE', 'UTF-8')
 
 module Triggers
-	def help(*args)
-		say "\x02Triggers:\x02 calc (c), imgur (ir, i), weather (w), translate (tl, t)"
-#		say "Triggers: \x02calc\x02 query \x0314(c)\x03, \x02imgur\x02 image_link_1 [image_link_2 ...] \x0314(ir)\x03, \x02weather\x02 location [fc:[0-3]]"
+	def help *args
+		say "Triggers: \x02calc\x02 (c), \x02imgur\x02 (ir, i), \x02weather\x02 (w), \x02translate\x02 (tl, t), \x02stocks\x02 (s), \x02define\x02 (d), \x02tell\x02"
 	end
 	
-#	def reload
-#		reload! if permission
-#	end
+	def reload
+		`killall -9 ruby; ruby -w bot.rb` if permissions
+	end
 	
-	def calc(args)
+	def calc args
 		response = JSON.parse(Net::HTTP.get(URI.parse("http://www.google.com/ig/calculator?hl=en&q=#{CGI.escape(args)}")).gsub(/([^,{]+):/,"\"\\1\":"))
 		if ['', '0'].include? response['error']
 			rhs = ($byte_killer.iconv response['rhs']).gsub(/ x26#215;\s10x3csupx3e(\-?\d+)x3c\/supx3e/,"e\\1").gsub(/x3csupx3e(\-?\d+)x3c\/supx3e/,"\^\\1")
@@ -33,25 +29,25 @@ module Triggers
 	end
 	alias_method :c, :calc
 	
-	def imgur(urls)
+	def imgur urls
 		urls.split(' ').each do |url|
 			if url.match(/^https?:\/\//)
-				response = Net::HTTP.post_form(URI.parse($api[:imgur][:url]), {
-					"key"	=> $api[:imgur][:key],
-					"image"	=> url,
-					"type"	=> "url"
+				response = Net::HTTP.post_form(URI.parse "http://api.imgur.com/2/upload.json", {
+					"key"	=>	$api_keys[:imgur],
+					"image"	=>	url,
+					"type"	=>	"url"
 				})
 				json = JSON.parse(response.body)
 				say url + " => " + json['upload']['links']['original']
 			else
-				onoez! "Bad URL: '#{url}'"
+				say "Bad URL: '#{url}'"
 			end
 		end
 	end
 	alias_method :ir, :imgur
 	alias_method :i, :imgur
 	
-	def weather(args)
+	def weather args
 		if args[-4..-1].match(/fc:([0-3])/)
 			fc_days = $1.to_i
 			location = args[0..-5]
@@ -59,7 +55,7 @@ module Triggers
 			fc_days = 0
 			location = args
 		end
-		weather = Hpricot::XML Net::HTTP.get URI.parse("http://www.google.com/ig/api?weather=#{URI.escape(location, /\W/)}")
+		weather = Hpricot::XML Net::HTTP.get URI.parse("http://www.google.com/ig/api?weather=#{CGI.escape(location, /\W/)}")
 		condition, temp_f, temp_c, humidity = (weather/:current_conditions/:*).map {|x| x.attributes['data']}[1,4]
 		reply = "Weather in \x02#{weather.at('city').attributes['data']}\x02: #{temp_f}\xb0F (#{temp_c}\xb0C); #{condition}; #{humidity}"
 		if fc_days > 0
@@ -74,7 +70,7 @@ module Triggers
 	end
 	alias_method :w, :weather
 	
-	def translate(args)
+	def translate args
 		lang_pair, lol, query = args.partition ' '
 		if lang_pair.match(/([a-z]{2})?\|[a-z]{2}/)
 			response = JSON.parse Net::HTTP.get URI.parse URI.escape "http://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=#{query}&langpair=#{lang_pair}"
@@ -95,7 +91,7 @@ module Triggers
 	alias_method :tl, :translate
 	alias_method :t, :translate
 	
-	def stocks(args)
+	def stocks args
 		r = Hpricot::XML Net::HTTP.get URI.parse "http://www.google.com/ig/api?stock=#{args}"
 		unless r.at(:company)['data'].empty?
 			say "Stock information for \x02#{r.at(:company)['data']}\x02 (#{r.at(:exchange)['data']}, #{r.at(:currency)['data']}): \x02Last\x02 #{r.at(:last)['data']}; \x02High\x02 #{r.at(:high)['data']}; \x02Low\x02 #{r.at(:low)['data']}; \x02Volume\x02 #{r.at(:volume)['data']}; \x02Change\x02 #{r.at(:change)['data']} (#{r.at(:perc_change)['data']}%)"
@@ -103,42 +99,42 @@ module Triggers
 			onoez! "\"#{args}\" is probably an invalid symbol"
 		end
 	end
-		
+	alias_method :s, :stocks
+	
+	def define
+	end
+	alias_method :d, :define
+	
+	def tell
+	end
 end
 
-class ThingDoer
-	def initialize(bot)
-		@bot = bot
-		
-		# login
-		cmd "USER MOSFET-#{bot.current_copy} 0 0 :Ruby-Passivated Junction"
-		cmd "NICK #{bot.nick}"
-		@bot.channels.each { |channel| cmd "JOIN #{channel}" }
+class RBot
+	@@copy = 0
+	def initialize server, port=6667, nick, user, channels
+		@socket = TCPSocket.new(server,port)
+		@nick, @channels, @current_copy, @user = nick, channels, (@@copy += 1), user
 	end
-	def cmd(str)
+	
+	def cmd str
 		puts "--> #{str}"
-		@bot.socket.send "#{str}\r\n", 0
+		@socket.send "#{str}\r\n", 0
 	end
-	def say(str, chan = @target)
+	
+	def say str, chan = @target
 		str.each_line do |line|
 			cmd "PRIVMSG #{chan} :#{line}"
 		end
 	end
+	
 	def onoez! reason=''
 		reply = "#{@nick}: I am error"
-		unless reason.empty?
-			reply << " (#{reason})"
-		end
+		reply << " (#{reason})" unless reason.empty?
 		say reply
 	end
 
-#	Two options for regexes. In all cases,
-#	-	$1 -> source hostmask*
-#	-	$2 -> command
-#	-	$3 -> target*
-#	-	$4 -> exists if channel message*
-#	-	$5 -> message
-#		
+#	Two options for regexes.
+#
 #	/(:\S+\s)?(\S+|\d{3})\s((#|&)?[\w\\\[\]{}\^`\|][\w\-\\\[\]{}\^`\|]*\s)?{1,2}:(.+)/
 #	-	matches all lines including numerical responses (\d{3}) (still in progress)
 #	
@@ -148,13 +144,13 @@ class ThingDoer
 	def handle(line)
 		begin
 			line.match(/(:\S+)? ?([A-Z]+) (#?[\w\-\\\[\]{}\^`\|]* )?:(.+)/)
-			@source, @raw_command, @target, @message = $1, $2, $3, $4, $5
+			@source, @raw_command, @target, @message = $1, $2, $3, $4
 			@nick = @source[/[^~!]+/] if @source != nil
 			self.send @raw_command.downcase! if @raw_command and @message
 		rescue NoMethodError
 			puts "*** [ThingDoer.handle] NoMethodError: #{$!}"
 		rescue ArgumentError
-			say "ArgumentError: #{$!}"
+			say "Argument Error: #{$!}"
 		end
 	end
 	
@@ -164,69 +160,54 @@ class ThingDoer
 			self.send command, args
 		end
 	end
+	
 	def ping
 		cmd "PONG :#{@message}"
 	end
+	
 	def notice
 	end
+	
 	def permission
 		return true if @source.match(/moshee@mo\.sh\.ee|24.16.155.210/)
 	end
 	
+	def connect
+		# login
+		cmd "USER RBOT-#{@current_copy} 0 0 :#{@user}"
+		cmd "NICK #{@nick}"
+		@channels.each { |channel| cmd "JOIN #{channel}" }
+		
+		# main loops
+		# read from socket
+		read_thread = Thread.new do
+			while true
+				begin			
+					ready = select([@socket], nil, nil)
+					redo if !ready
+					break if @socket == nil
+					line = @socket.gets
+					puts "<-- #{line.chomp!}"
+					handle line
+				rescue Exception
+					puts "*** #{$!}"
+					retry
+				end
+			end
+		end
+		write_thread = Thread.new do
+			while true
+				cmd gets.chomp
+			end
+		end
+		read_thread.join
+		write_thread.join
+	end
+	
 	private
+	
 	include Triggers
 end
 
-class Bot
-	@@copy = 0
-	attr_reader :socket, :nick, :channels, :current_copy
-	def initialize(server, port=6667, nick, channels)
-		@socket = TCPSocket.new(server,port)
-		@nick, @channels, @current_copy = nick, channels, (@@copy += 1)
-		please = ThingDoer.new(self)
-		
-		# main loop
-		while true
-			begin			
-				ready = select([@socket], nil, nil)
-				redo if !ready
-				break if @socket == nil
-				line = @socket.gets
-				puts "<-- #{line.chomp!}"
-				please.handle line
-			rescue Exception
-				puts "*** #{$!}"
-				retry
-			end
-		end
-	end
-end
-
-this = Bot.new("irc.rizon.net", 6667, "mosfet`rb", ["#moshee"])
-
-=begin
-
-# test code (without socket)
-
-while true
-	begin
-		puts
-		print ">> "
-		input = gets.chomp.split(" ")
-		command = input.shift.intern
-		unless this.disallowed_commands.include? command
-			if input.empty?
-				this.send(command)
-			else
-				this.send(command, input)
-			end
-		end
-	rescue NoMethodError
-		puts "No such method '#{command}': " + $!
-		retry
-	rescue ArgumentError
-		puts "Argument error for '#{command}': " + $!
-		retry
-	end
-end
-=end
+this = RBot.new("irc.rizon.net", 6667, "mosfet`rb", "Ruby-Passivated Junction", ["#moshee"])
+this.connect
